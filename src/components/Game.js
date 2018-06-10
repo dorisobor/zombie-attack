294c4d14a9d79f6cb821faa8c70d497df145a580
dorisobor <troyamber@live.com>
2018-06-10 23:13:40 +0200

    fixed broken gif

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from "react";
+//imports the gif image as zombieWalking
+import zombieWalking from "./../images/zombie-walking.gif";
 
 
-export default () => (
+class Game extends Component {
+  render() {
+    return (
 <div className = "game-container">
 <h2>Fight the zombies</h2>
-
+<div className="zombie-walking">
+<img src={zombieWalking} alt="walking-zombie"/>
+</div>
   </div>
+
 );
+}
+}
+
+export default Game;
