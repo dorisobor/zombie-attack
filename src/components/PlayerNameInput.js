9814b889c1aaadb04f67a66b617cddf2f23898bf
dorisobor <troyamber@live.com>
2018-06-12 15:55:23 +0200

    updated components

---
+++ src/components/PlayerNameInput.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class playerName extends Component {
+    render() {
+      return (
+  <div className = "game-container">
+  <h2>Fight the zombies</h2>
+  <div className="zombie-walking">
+  </div>
+    </div>
+  
+  );
+  }
+  }
+  
+  export default playerName;
