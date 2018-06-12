a3857c97838ec322000a0b369329e4b93bd6ad61
dorisobor <troyamber@live.com>
2018-06-12 20:03:50 +0200

    fixed display input field error

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
+import Player from "./PlayerNameInput";
 
-import playerName from "./PlayerNameInput";
 
 //start button will lead to the game 
 
 export default ({ history }) => (
 <div className = "start-page-container">
 <h1>Zombie Attack</h1>
-<div className="player-name-input">{playerName}</div>
+
 <button onClick={()=> history.push("/Game")}>
  START THE GAME
 </button>
+
+<div className = "form-container"> 
+ <Player /></div>
   </div>
+
+
 );
--- src/components/PlayerNameInput.js
+++ src/components/PlayerNameInput.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 
 
-class playerName extends Component {
-    render() {
-      return (
-  <div className = "game-container">
-  <h2>Fight the zombies</h2>
-  <div className="zombie-walking">
-  </div>
-    </div>
-  
-  );
+
+
+
+class PlayerName extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
   }
-  }
   
-  export default playerName;
+  export default PlayerName;
