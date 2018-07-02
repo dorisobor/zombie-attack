f3628bb12d1d38a88b4046d138e7c57e04adb496
dorisobor <troyamber@live.com>
2018-07-03 00:32:39 +0200

    added working counter

---
+++ src/components/Counter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import zombieWalking from "./../images/zombie-walking.gif";
+
+
+class Counter extends Component {
+
+    state = {
+        counter: 0
+      }
+
+
+    counterClick = () => {
+        this.setState({ counter: this.state.counter + 1});
+        
+      }
+      render () {
+        return <div>
+          <p>${this.state.counter}</p>
+        <div className="zombie-walking">
+        <img src={zombieWalking} alt="walking-zombie" onClick={this.counterClick}/>
+        </div>
+          {/* <button onClick={this.counterClick}>+</button> */}
+        </div>
+      }
+    
+    
+}
+
+
+export default Counter;
