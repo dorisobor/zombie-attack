b1f4f2cc1578b1ac0fd3b2ac1d070fe5c162a946
dorisobor <troyamber@live.com>
2018-07-03 19:56:44 +0200

    added a test multipler to uppgrade

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
 import zombieWalking from "./../images/zombie-walking.gif";
+import strenghtBoostBottle from "./../images/water.png";
+
 
 class Counter extends Component {
 
     // the counter will start at zero in the first state
     state = {
-        counter: 0,
-        water:15,
+        moneyScore: 0,
+        counter: 1,
+        strenghtBoostPrice: 15,
+        //click:1,
       }
 
     // function for the counter everytime you click it will add 1
     counterClick = () => {
-        this.setState({ counter: this.state.counter + 1});
+        this.setState({moneyScore: this.state.moneyScore + this.state.counter});
         }
 
-     waterCounterClick = () => {
-       if (this.state.counter - this.state.water >= 0){
-      this.setState({ counter: this.state.counter - this.state.water});
+     strenghtBoostCounterClick = () => {
+       if (this.state.moneyScore - this.state.strenghtBoostPrice >= 0){
+          this.setState({ 
+            moneyScore: this.state.moneyScore - this.state.strenghtBoostPrice,
+            counter: this.state.counter + 1
+            //strenghtBoostPrice: Math.round(this.state.strenghtBoostPrice * 1.1) 
+          });
+
     }
     }
       render () {
         return(
         <div>
-          <p>${this.state.counter}</p>
-          <button onClick={this.waterCounterClick}>x</button>
+          <p>${this.state.moneyScore}</p>
+          <img src={strenghtBoostBottle} alt="strenght-boost-bottle" onClick={this.strenghtBoostCounterClick}/>
         <div className="zombie-walking">
         <img src={zombieWalking} alt="walking-zombie" onClick={this.counterClick}/>
         </div>
