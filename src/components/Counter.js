a9e2abf38c0f954b2386d2b27ad4640e20b64dc2
dorisobor <troyamber@live.com>
2018-07-05 17:29:29 +0200

    added increased price after each buy of upgrade

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,9 @@ import strenghtBoostBottle from "./../images/water.png";
 
 class Counter extends Component {
 
-    // the counter will start at zero in the first state
+    // the money score will start at zero in the first state
+    //the counter will add 1 after each click
+    //every item has a price value that increases the current state money score
     state = {
         moneyScore: 0,
         counter: 1,
@@ -13,17 +15,17 @@ class Counter extends Component {
         //click:1,
       }
 
-    // function for the counter everytime you click it will add 1
+    // function for the money score everytime you click it will add 1
     counterClick = () => {
         this.setState({moneyScore: this.state.moneyScore + this.state.counter});
         }
-
+    // everytime you buy an upgrade the price and the clicks/hits will increase
      strenghtBoostCounterClick = () => {
        if (this.state.moneyScore - this.state.strenghtBoostPrice >= 0){
           this.setState({ 
             moneyScore: this.state.moneyScore - this.state.strenghtBoostPrice,
             counter: this.state.counter + 1,
-            //strenghtBoostPrice: Math.round(this.state.strenghtBoostPrice * 1.1) 
+            strenghtBoostPrice:  this.state.strenghtBoostPrice * 2 
           });
 
     }
@@ -32,8 +34,16 @@ class Counter extends Component {
         return(
         <div>
           <p>${this.state.moneyScore}</p>
+          <div className="uppgrade-list">
+          <div className="strenght-boost">
+
           <img src={strenghtBoostBottle} alt="strenght-boost-bottle" onClick={this.strenghtBoostCounterClick}/>
+          <p>${this.state.strenghtBoostPrice}</p>
+          </div>
+          </div>
+          <p>{this.state.counter}hits</p>
         <div className="zombie-walking">
+        
         <img src={zombieWalking} alt="walking-zombie" onClick={this.counterClick}/>
         </div>
         </div>
