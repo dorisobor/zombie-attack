d991e916f5fb98142ef9e3fa18221c20d434fb40
dorisobor <troyamber@live.com>
2018-07-09 18:42:49 +0200

    added intro text

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,13 @@ export default (props) => (
     <h1 className="display-4">Zombie Bounty<span className="hunt-headline">HUNT</span></h1>
     <div className="container">
 
-    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id fringilla sem. Quisque eu gravida tellus, eu dignissim ligula. Nunc vehicula efficitur nulla, ut fermentum magna auctor et. Curabitur vulputate erat quis augue sagittis, sit amet facilisis orci laoreet. Nam porta sapien convallis nunc faucibus, id blandit enim ultrices. Integer aliquet arcu odio, sed bibendum turpis aliquet eget. . </p>
+    <p className="lead">The world has been infected by Zombies.The world's population 
+    has decreased and it's becoming more and more dangerous. Luckily humanity can rely on the brave zombie 
+    bounty hunters who will risk their lives...for a big price. Are you one of them? 
+    Join the bounty hunt and get rewared with big bucks!</p>
+    <p className="lead"><span>Instructions</span>
+    <br /> Collect and gain bounty money by clicking on the zombie.</p>
+    <p className="lead"><span>Good luck and be careful out there,&nbsp;friend!</span></p>
   
     </div>
     <div className="form-container">
